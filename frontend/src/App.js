5cffa6566c47a45da283c18d9a9ab7988025f691
Sandro Mejia <sandromejia3409@gmail.com>
2023-03-11 22:03:02 -0500

    added axios fetch funtion for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [file, setFile] = useState("");
+  const formData = new FormData();
+  formData.append("file", file[0]);
+
+  const handleSubmit = async () => {
+    try {
+      const data = await axios.post("http://localhost:5000/convert", formData);
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
     console.log(file);
@@ -10,6 +23,7 @@ function App() {
   return (
     <div>
       <input type="file" onChange={(e) => setFile(e.target.files)} />
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
